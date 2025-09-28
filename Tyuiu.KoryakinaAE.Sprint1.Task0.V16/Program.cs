d635d7234c91ae5ab2c3a723e6d08e5b156d8152
korya <korya@COOLCOMP>
2025-09-28 22:27:51 +0500

    Add project files.

---
+++ Tyuiu.KoryakinaAE.Sprint1.Task0.V16/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.KoryakinaAE.Sprint1.Task0.V16.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Корякина А. Э. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы с C#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #16                                                              *");
+        Console.WriteLine("* Выполнил: Корякина Алина Эльдаяговна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение 6/2/3+1-6/2*3           *");
+        Console.WriteLine("* и печатает результат на экране                                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 6/2/3+1-6/2*3                                                            ");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate());
+
+        Console.ReadLine();
+    }
+}
