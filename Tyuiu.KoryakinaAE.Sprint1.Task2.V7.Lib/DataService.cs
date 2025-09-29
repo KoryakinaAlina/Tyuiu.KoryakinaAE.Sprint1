7ac68831bc0ae4254e4c3f0f63c16eb81de0e683
KoryakinaAlina <koryakina.alina2007ya.ru@yandex.ru>
2025-09-29 22:33:50 +0500

    Update DataService.cs

--- Tyuiu.KoryakinaAE.Sprint1.Task2.V7.Lib/DataService.cs
+++ Tyuiu.KoryakinaAE.Sprint1.Task2.V7.Lib/DataService.cs
@@ -1,13 +1,14 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint1;
+using tyuiu.cources.programming.interfaces.Sprint1;
+using System;
 
 namespace Tyuiu.KoryakinaAE.Sprint1.Task2.V7.Lib
 {
     public class DataService : ISprint1Task2V7
     {
-
         public double CalculateSquareCircle(int value)
         {
-            return Math.PI * value * value;
+            double result = Math.PI * value * value;
+            return Math.Round(result, 3);
         }
     }
 }
