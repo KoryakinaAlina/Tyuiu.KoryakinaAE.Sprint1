a3032f6e75e8eaad4f276d54ff386cf9f0780cb6
KoryakinaAlina <Tyuiu.KoryakinaAE@outlook.com>
2025-10-02 01:25:28 +0500

    Task4

---
+++ Tyuiu.KoryakinaAE.Sprint1.Task4.V26.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KoryakinaAE.Sprint1.Task4.V26.Lib;
+namespace Tyuiu.KoryakinaAE.Sprint1.Task4.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 1;
+            double wait = 0.105;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KoryakinaAE.Sprint1.Task4.V26/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.KoryakinaAE.Sprint1.Task4.V26.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1  | Выполнил: Корякина А.Э. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #26                                                              *");
+        Console.WriteLine("* Выполнил: Корякина А.Э | ИСТНб-25-1                                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine();
+        int x;
+        Console.WriteLine("Введите значение Х:");
+        x = Convert.ToInt32(Console.ReadLine());
+        int y;
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("arctgx + y / e ^ y + x = " + ds.Calculate(x, y));
+        Console.ReadKey();
+
+    }
+}
